1755536715fe10a61dbe3cda146d63cf9c27ffe3
BlankP0rtal <arjunasarathy.m@zohocorp.com>
2018-12-03 13:28:05 +0530

    new Branch Commit

--- JavaStash/Phase - 2/com/Test/Types/SingleTonClass.java
+++
@@ -1,9 +0,0 @@
-package com.Test.Types;
-
-public class SingleTonClass{
-	public static SingleTonClass singleTon = new SingleTonClass();
-	public String singleVar;
-	private SingleTonClass(){
-		//dummyt
-	}
-}
